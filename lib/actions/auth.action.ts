abcad65d981888366f9de1a3a374d16ceb4936f2
ArkarPhyo <arkarphyo.akp1996@gmail.com>
2025-05-02 14:26:06 +0700

    [impl] Show generated interviews on home page

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -122,3 +122,43 @@ export async function isAuthenticated() {
 
     return !!user;
 }
+
+export async function getInterviewsByUserId(userId: string): Promise<Interview[] | null> {
+    try {
+        const interviews = await db
+            .collection("interviews")
+            .where("userId", "==", userId)
+            .orderBy("createdAt", "desc")
+            .get();
+        return interviews.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        })) as Interview[];
+
+    } catch (error) {
+        console.error("Error getting interview by user ID:", error);
+        return null;
+    }
+};
+
+export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null> {
+    try {
+        const { userId, limit = 20 } = params;
+
+        const interviews = await db
+            .collection("interviews")
+            .orderBy("createdAt", "desc")
+            .where("finalize", "==", true)
+            .where("userId", "!=", userId)
+            .limit(limit)
+            .get();
+
+        return interviews.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        })) as Interview[];
+    } catch (error) {
+        console.error("Error getting latest interviews: ", error);
+        return null;
+    }
+}
